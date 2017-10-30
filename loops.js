84be381c123e2241faaf229ac30120a73edd638d
Robert Meis <robert.meis@colorado.edu>
2017-10-30 18:48:52 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ function forLoop(array) {
 
 for (var i = 0; i <= 25; i++) {
 
-  array.push("I am a ${i} strange loop${i === 0 ? '' : 's'}.");
+  array.push("I am ${i} strange loop${i === 0 ? '' : 's'}.");
 }
 return array;
 }
